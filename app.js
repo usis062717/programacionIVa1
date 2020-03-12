194bd2e4c6fff18783aacbc27541cb222aca8c21
Gabriela_Rivas <gabriela.rivas88@hotmail.es>
2020-03-12 09:11:08 -0600

    app alumnos ofline

--- app.js
+++ app.js
@@ -36,13 +36,3 @@ document.addEventListener("DOMContentLoaded", (event) =>{
 
     });
 });
-
-/*document.addEventListener("DOMContentLoaded",init);*/
-
-/*document.addEventListener("DOMContentLoaded",function(event){
-    alert("Pagina cargo forma 2");
-});*/
-
-/*function init(event){
-    alert("Hola la pagina a cargado");
-}*/
