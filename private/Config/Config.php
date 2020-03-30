4dd97f97f8372339cc9469026fc484610dc20265
Gabriela_Rivas <gabriela.rivas88@hotmail.es>
2020-03-29 19:13:33 -0600

    app

---
+++ private/Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+include('../../Conexion/DB.php');
+
+$conexion = new Conexion('localhost','root','','db_app_academica');
+
+
+?>
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,12 @@
-var $ = el => document.querySelector(el);
-document.addEventListener("DOMContentLoaded",event=>{
-    let mostrarVista = $("[class*='mostrar']");
+
+    function init(){
+        var $ = el => {
+            return el.match(/^#/) ? document.querySelector(el) : document.querySelectorAll(el);
+        }
+    let mostrarVista = $("[class*='mostrar-alumno']");
+    mostrardocente= $("[class*='mostrar-docente']");
     mostrarVista.addEventListener('click',e=>{
+        console.log(mostrarVista);
         e.stopPropagation();
 
         let modulo = e.toElement.dataset.modulo;
@@ -19,4 +24,23 @@ document.addEventListener("DOMContentLoaded",event=>{
             cuerpo.appendChild(script);
         });
     });
-});
+
+    mostrardocente.addEventListener('click',e=>{
+         e.stopPropagation();
+
+        let modulo = e.srcElement.dataset.modulo;
+        fetch('public/vistas/Docentes/docentes.html').then( resp=>resp.text() ).then(resp=>{
+            $(`#vista-${modulo}`).innerHTML = resp;
+            
+            let btnCerrar = $(".close");
+            btnCerrar.addEventListener("click",event=>{
+                $(`#vista-${modulo}`).innerHTML = "";
+            });
+
+            let cuerpo = $("body"),
+                script = document.createElement("script");
+            script.src = `public/vistas/${modulo}/${modulo}.js`;
+          cuerpo.appendChild(script);
+        }); 
+    });
+};
